808e475a22d6aacf67969bb830043a7fddaebb1b
Alexandr <sokolow.a.v@gmail.com>
2021-07-03 04:52:46 +0300

    menu.js was fixed, topping adaptation was fixed

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -3,5 +3,6 @@ $(document).ready(function() {
     $('.header-nav__burger').click(function() {
         $('.header-nav__burger').toggleClass('open-menu');
         $('.header-nav__mobile-list').toggleClass('open-menu');
+        $('body').toggleClass('fixed');
     });
 });
