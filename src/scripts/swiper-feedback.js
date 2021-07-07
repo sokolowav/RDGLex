3d0dfe847bb3e385f469f9f1fe83b516b776bd01
Alexandr <sokolow.a.v@gmail.com>
2021-07-07 20:22:03 +0300

    swiper-feedback.js was added

--- src/scripts/swiper-feedback.js
+++ src/scripts/swiper-feedback.js
@@ -1,6 +1,6 @@
-console.log('swiper.js added')
+console.log('swiper-feedback.js added')
 
-import Swiper from 'https://unpkg.com/swiper/swiper-bundle.esm.browser.min.js'
+//import Swiper from 'https://unpkg.com/swiper/swiper-bundle.esm.browser.min.js'
 
   //const swiperInit = () => {
   //  const containers = document.querySelectorAll('.swiper-container')
@@ -43,20 +43,32 @@ import Swiper from 'https://unpkg.com/swiper/swiper-bundle.esm.browser.min.js'
   //
   //export default swiperInit
 
-  const pagination = document.querySelector('.swiper-pagination')
-  const swiper = new Swiper('.swiper-container', {
-    direction: 'horizontal',
-    loop: true,
-    slidesPerView: 1,
-  
-    pagination: {
-      el: pagination,
-      clickable: true,
-    },
-  
-    navigation: {
-      nextEl: '.swipers-arrows__right',
-      prevEl: '.swipers-arrows__left',
-    },
-  
-  });
+    const pagination = document.querySelector('.feedback-nav-numbers')
+    let menu = ['1', '2', '3', '4', '5', '6', '7']
+    const swiper = new Swiper('.swiper-container', {
+      direction: 'horizontal',
+      loop: true,
+      slidesPerView: 1,
+      spaceBetween: 50,
+      pagination: {
+        el: pagination,
+        clickable: true,
+        bulletClass: 'feedback-nav-number',
+        bulletActiveClass: 'feedback-nav-number__active',
+        renderBullet: function (index, className) {
+          return '<span class="' + className + '">' + (menu[index]) + '</span>';
+        },
+      },
+    
+      navigation: {
+        nextEl: '.swiper-feedback-arrows__right',
+        prevEl: '.swiper-feedback-arrows__left',
+      },
+    
+    });
+    
+  if (window.innerWidth > 1023) {
+    swiper.enable()
+    } else {
+      swiper.disable()
+  };
