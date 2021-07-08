7fc019a235ed0b4133c148867ba9661b65802222
Alexandr <sokolow.a.v@gmail.com>
2021-07-08 22:19:36 +0300

    show-more-news.js was added

---
+++ src/scripts/show-more-feedback.js
@@ -0,0 +1,8 @@
+console.log('show-more.js added')
+
+const btn = document.querySelector('.show-more-button')
+btn.addEventListener('click', () => {
+    let showMoreBlock = document.querySelector('.show-more__hidden');
+    showMoreBlock.classList.remove('show-more__hidden');
+    showMoreBlock.classList.add('show-more__hidden-hight-width');
+});
