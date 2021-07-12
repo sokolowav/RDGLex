0f8ff5d34d7decf7d61a2868577a8e26c7409c54
Alexandr <sokolow.a.v@gmail.com>
2021-07-12 19:46:34 +0300

    modals were corrected, read-more.js was added

---
+++ src/scripts/read-more.js
@@ -0,0 +1,23 @@
+console.log('read-more.js added')
+
+const feedback = document.querySelectorAll('#feedback-item-info');
+
+feedback.forEach( el => {
+    const btnShow = el.querySelector('.read-more-button__show');
+    const btnHide = el.querySelector('.read-more-button__hide');
+    const limiter = el.querySelector('.limiter');
+    const bottom = el.querySelector('.bottom');
+    
+    btnShow.addEventListener('click', () => {
+        limiter.classList.add('limiter__show')
+        bottom.classList.add('bottom__hide')
+        btnShow.classList.add('hidden')
+        btnHide.classList.remove('hidden')
+    });
+    btnHide.addEventListener('click', () => {
+        limiter.classList.remove('limiter__show')
+        bottom.classList.remove('bottom__hide')
+        btnShow.classList.remove('hidden')
+        btnHide.classList.add('hidden')
+    });
+})
