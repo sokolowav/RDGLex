71dbcf9c0dbaa742eb437de8f7d4a4cf66db2508
Alexandr <sokolow.a.v@gmail.com>
2021-07-19 18:49:56 +0300

    accordion-service.js was redacted

--- build/scripts/accordion-service.js
+++ build/scripts/accordion-service.js
@@ -2,14 +2,12 @@ console.log('accordion-service.js added')
 
 const acc = document.querySelectorAll('.accordion-service')
 
-console.dir(acc)
-
-for (let i = 0; i < acc.length; i++) {
-	acc[i].addEventListener('click', function () {
+acc.forEach(el => {
+	el.addEventListener('click', function () {
 		this.classList.toggle('accordion__active')
 		const panel = this.nextElementSibling
 		panel.style.maxHeight
 			? (panel.style.maxHeight = null)
 			: (panel.style.maxHeight = panel.scrollHeight + 'px')
 	})
-}
+})
