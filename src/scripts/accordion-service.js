5311b50883471d882eb637283689273a2a95a5f8
Alexandr <sokolow.a.v@gmail.com>
2021-07-19 15:36:51 +0300

    menu.js and accordion-service.js were redacted

--- src/scripts/accordion-service.js
+++ src/scripts/accordion-service.js
@@ -1,15 +1,15 @@
 console.log('accordion-service.js added')
 
-const acc = document.getElementsByClassName('accordion-service')
+const acc = document.querySelectorAll('.accordion-service')
+
+console.dir(acc)
 
 for (let i = 0; i < acc.length; i++) {
 	acc[i].addEventListener('click', function () {
 		this.classList.toggle('accordion__active')
 		const panel = this.nextElementSibling
-		if (panel.style.maxHeight) {
-			panel.style.maxHeight = null
-		} else {
-			panel.style.maxHeight = panel.scrollHeight + 'px'
-		}
+		panel.style.maxHeight
+			? (panel.style.maxHeight = null)
+			: (panel.style.maxHeight = panel.scrollHeight + 'px')
 	})
 }
--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,8 +1,11 @@
 console.log('menu.js added')
-$(document).ready(function () {
-	$('.header-nav__burger').click(function () {
-		$('.header-nav__burger').toggleClass('open-menu')
-		$('.header-nav__mobile-list').toggleClass('open-menu')
-		$('body').toggleClass('fixed')
-	})
+
+const burger = document.querySelector('.header-nav__burger')
+
+burger.addEventListener('click', () => {
+	const body = document.getElementsByTagName('body')
+	const mobileList = document.querySelector('.header-nav__mobile-list')
+	burger.classList.toggle('open-menu')
+	mobileList.classList.toggle('open-menu')
+	body[0].classList.toggle('fixed')
 })
