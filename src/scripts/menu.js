891ee8d7027c45507969ef8d5e3a7531ddc05bf5
Alexandr <sokolow.a.v@gmail.com>
2021-07-21 01:44:03 +0300

    menu.js was optimized

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -3,9 +3,10 @@ console.log('menu.js added')
 const burger = document.querySelector('.header-nav__burger')
 
 burger.addEventListener('click', () => {
-	const body = document.getElementsByTagName('body')
+	const body = document.querySelector('body')
 	const mobileList = document.querySelector('.header-nav__mobile-list')
+
 	burger.classList.toggle('open-menu')
 	mobileList.classList.toggle('open-menu')
-	body[0].classList.toggle('fixed')
+	body.classList.toggle('fixed')
 })
